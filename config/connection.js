391cce4d6ff47e2600902ff5e9c3fc3b827493f6
Ala Owerri <irete@alaowerri.com>
2022-10-03 19:35:58 -0400

    add dialectoption script

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,14 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {dialectOptions: {
+    ssl: {
+        rejectUnauthorized: false
+    }
+}});
+  
+} 
+else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
