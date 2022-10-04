80a784a7b07f2baf262a9122eea9f1c74a65de1a
Ala Owerri <irete@alaowerri.com>
2022-10-04 09:44:28 -0400

    css and javascript files in public folder

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,13 +2,14 @@ const newFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#project-name').value.trim();
+  const etype = document.querySelector('#project-type').value.trim();
   const needed_funding = document.querySelector('#project-funding').value.trim();
   const description = document.querySelector('#project-desc').value.trim();
 
-  if (name && needed_funding && description) {
+  if (name && etype && needed_funding && description) {
     const response = await fetch(`/api/projects`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ name, etype, needed_funding, description }),
       headers: {
         'Content-Type': 'application/json',
       },
